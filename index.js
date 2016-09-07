008c53c210603404c2e5d8996a879ecbf9252c7e
Elon <elonrubin@gmail.com>
2016-09-07 13:25:04 -0400

    Fixing file path issue
    Trying to fix file path issue per server feedback

--- index.js
+++ index.js
@@ -21,13 +21,13 @@ var api = new ParseServer({
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   },
   push: {
-	  ios: [
+	  ios: 
  			{
-       			 pfx: '/Users/elonrubin/Dropbox/Companies/Vennn/Misc/Certificates/ParsePushDevelopmentCertificate.p12', // Prod PFX or P12
+       			 pfx: 'Users/elonrubin/Dropbox/Companies/Vennn/Servers/parsepushcert.p12', // Prod PFX or P12
         		bundleId: 'io.vennn.Venn',  
         		production: true // Prod
   	    		}  
-  			]
+  			
   		}
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
